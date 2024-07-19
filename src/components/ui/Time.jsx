f21848e9b9e1231f1e961c218fa4c16fa38d7466
Tiga Shabani <tigana.saabani@gmail.com>
2024-07-19 15:37:43 +0200

    Use Chakra UI

--- src/components/ui/Time.jsx
+++ src/components/ui/Time.jsx
@@ -1,19 +1,19 @@
-
-import './Time.css'
+import { Heading } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 
 export const Time = () => {
   const [currentTime, setCurrentTime] = useState(
     new Date().toLocaleTimeString("nl-NL")
   );
-
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentTime(new Date().toLocaleTimeString("nl-NL"));
     }, 1000);
-    // Cleanup the interval on component unmount
     return () => clearInterval(timer);
   }, []);
-
-  return <h3>{currentTime}</h3>;
+  return (
+    <Heading as="h4" size="sm" mt={4} color="darkblue" textAlign="left">
+      {currentTime}
+    </Heading>
+  );
 };
